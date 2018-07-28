a619d2653fb4e11d87649ea982706d42d9fe6d3d
Ihsara <longchau21@gmail.com>
2018-07-28 17:29:47 +0200

    Add page header

--- app.py
+++ app.py
@@ -11,9 +11,44 @@ import pandas as pd
 '''
     Declaration of a Dash-app. Always right after import.
 '''
-app = dash.Dash()
+app = dash.Dash(
+    meta_tags=[
+    {
+        'name': 'description',
+        'content': 'Điểm thi THPT qua góc nhìn của đồ thị - biểu đồ. Bằng cách sử dụng biểu đồ, trang web này hướng đến mục tiêu cung cấp một cách nhìn khách quan về điểm thi tốt nghiệp THPT 2018'
+    },
+    {
+        'name': 'author',
+        'content': 'Trần Long Châu'
+    },
+    {
+        'charset': 'utf-8'
+    }
+]
+)
 server = app.server
 app.config.suppress_callback_exceptions = True
+
+app.index_string = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        <div>My Custom header</div>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+        </footer>
+        <div>My Custom footer</div>
+    </body>
+</html>
+'''
 
 #CONSTANTS declaration
 __file__ = "dat/province_code.xlsx"
@@ -32,8 +67,85 @@ def get_data(ref_province):
 province_ref = pd.read_excel(__file__)
 data_wrapper = get_data(province_ref)
 
+# reusable componenets
+def make_dash_table(df):
+    ''' Return a dash definitio of an HTML table for a Pandas dataframe '''
+    table = []
+    for index, row in df.iterrows():
+        html_row = []
+        for i in range(len(row)):
+            html_row.append(html.Td([row[i]]))
+        table.append(html.Tr(html_row))
+    return table
+
+
+def print_button():
+    printButton = html.A(['Print PDF'],className="button no-print print",style={'position': "absolute", 'top': '-40', 'right': '0'})
+    return printButton
+
+# includes page/full view
+def get_logo():
+    logo = html.Div([
+
+        html.Div([
+            html.Img(src='http://logonoid.com/images/vanguard-logo.png', height='40', width='160')
+        ], className="ten columns padded"),
+
+        html.Div([
+            dcc.Link('Full View   ', href='/full-view')
+        ], className="two columns page-view no-print")
+
+    ], className="row gs-header")
+    return logo
+
+
+def get_header():
+    header = html.Div([
+
+        html.Div([
+            html.H5(
+                'Vanguard 500 Index Fund Investor Shares')
+        ], className="twelve columns padded")
+
+    ], className="row gs-header gs-text-header")
+    return header
+
+
+def get_menu():
+    menu = html.Div([
+
+        dcc.Link('Overview   ', href='/overview', className="tab first"),
+
+        dcc.Link('Price Performance   ', href='/price-performance', className="tab"),
+
+        dcc.Link('Portfolio & Management   ', href='/portfolio-management', className="tab"),
+
+        dcc.Link('Fees & Minimums   ', href='/fees', className="tab"),
+
+        dcc.Link('Distributions   ', href='/distributions', className="tab"),
+
+        dcc.Link('News & Reviews   ', href='/news-and-reviews', className="tab")
+
+    ], className="row ")
+    return menu
+
 layout = html.Div( [
-    html.H1(children='Trang đồ họa đồ thị kết quả thi THPT 2018'),
+    html.Div(
+        className="app-header",
+        children=[
+            html.H1(className="app-header--title", children='Trang đồ họa đồ thị kết quả thi THPT 2018'),
+        ]
+    ),•FeKX(6*
+
+    html.Div([
+        get_logo(),
+        get_header(),
+        html.Br([]),
+        get_menu(),
+    ]
+    ),
+
+
 
     html.P(children='''
         Chào mừng bạn đã đến với trang chủ của trang
--- index.py
+++ index.py
@@ -10,11 +10,12 @@ import pandas as pd
 from app import app, server, layout
 from apps import app1, app2
 
+#page layout
+noPage = html.Div([  # 404
 
-#Add CSS custom files here
-app.css.append_css({
-    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
-})
+    html.P(["404 Page not found"])
+
+    ], className="no-page")
 
 #Base app layout:
 app.layout = html.Div(children=[
@@ -24,6 +25,17 @@ app.layout = html.Div(children=[
 
 ])
 
+#Add CSS custom files here
+external_css = ["https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css",
+                "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
+                "//fonts.googleapis.com/css?family=Raleway:400,300,600",
+                "https://codepen.io/bcd/pen/KQrXdb.css",
+                'https://codepen.io/chriddyp/pen/bWLwgP.css',
+                "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"]
+
+for css in external_css:
+    app.css.append_css({"external_url": css})
+
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
@@ -31,8 +43,10 @@ def display_page(pathname):
         return app1.layout
     elif pathname == '/apps/app2':
         return app2.layout
-    else:
+    elif pathname == '/':
         return layout
+    else:
+        return noPage
 
 if __name__ == '__main__':
     app.run_server(debug=True)
